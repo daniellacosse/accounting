4dd649195ff2e81c810f9a3450dcfef5e4311c0d
D@nielLaCos.se <d@niellacos.se>
2019-09-01 19:15:30 -0400

    .buildfiles integration (#54)
    * buildfile integration
    
    * update circle job
    
    * okay, 3rd time's a charm
    
    * fix ifenv
    
    * throw in some content changes for good measure
    
    * new buildfiles, some config and make adjustments
    
    * overrides package name
    
    * buildfiles wildcard
    
    * succesfully pull submodules
    
    * i wonder if that space was funky-ing it up
    
    * ehhh fuck it let's try
    
    * update/cache buildfiles
    
    * we're not running yarn install LOL
    
    * make make folder first
    
    * remove precommit

--- .babelrc.js
+++ .babelrc.js
@@ -1,17 +1,1 @@
-const presets = [
-  ["@babel/preset-env", { targets: { esmodules: true } }],
-  "@babel/preset-typescript"
-];
-
-const plugins = [
-  [
-    "module-resolver",
-    {
-      extensions: [".ts"],
-      root: "."
-    }
-  ],
-  "convert-to-json"
-];
-
-module.exports = { presets, plugins };
+.buildfiles/defaults/.babelrc.js
